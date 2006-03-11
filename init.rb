7cb3d3383d239025766acc37cd02d0512b6d0b02
jonathan <jonathan@20afb1e0-9c0e-0410-9884-91ed27886737>
2006-03-11 05:13:21 +0000

    git-svn-id: http://svn.viney.net.nz/things/rails/plugins/validates_date_time@14 20afb1e0-9c0e-0410-9884-91ed27886737

--- init.rb
+++ init.rb
@@ -1,3 +1,3 @@
-require File.dirname(__FILE__) + '/lib/validates_date'
+require File.dirname(__FILE__) + '/lib/validates_date_time'
 
 ActiveRecord::Base.send(:include, ActiveRecord::Validations::DateTime)
