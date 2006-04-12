c5e72a6b88688ecbdbf0a17356e7e8b5152f37a1
jonathan <jonathan@20afb1e0-9c0e-0410-9884-91ed27886737>
2006-04-12 23:02:19 +0000

    git-svn-id: http://svn.viney.net.nz/things/rails/plugins/validates_date_time@42 20afb1e0-9c0e-0410-9884-91ed27886737

--- test/fixtures/person.rb
+++ test/fixtures/person.rb
@@ -5,4 +5,6 @@ class Person < ActiveRecord::Base
   validates_date :date_of_arrival, :if => Proc.new { |p| p.date_of_arrival? }, :before => Proc.new { |p| p.date_of_departure }, :before_message => "avant %s", :after_message => "apres %s"
   
   validates_time :time_of_birth, :if => Proc.new { |p| p.time_of_birth? }
+  
+  validates_datetime :date_and_time, :if => Proc.new { |p| p.date_and_time? }
 end
