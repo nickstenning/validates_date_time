bcba77d140bf78df793b0dafdf0051e45d629450
jonathan <jonathan@20afb1e0-9c0e-0410-9884-91ed27886737>
2006-05-03 13:05:36 +0000

    git-svn-id: http://svn.viney.net.nz/things/rails/plugins/validates_date_time@47 20afb1e0-9c0e-0410-9884-91ed27886737

--- lib/validates_date_time.rb
+++ lib/validates_date_time.rb
@@ -4,79 +4,114 @@ module ActiveRecord::Validations::DateTime
     base.extend(ClassMethods)
   end
   
+  class DateParseError < StandardError #:nodoc:
+  end
+  class TimeParseError < StandardError #:nodoc:
+  end
+  class DateTimeParseError < StandardError #:nodoc:
+  end
+  
+  mattr_accessor :us_date_format
+  us_date_format = false
+  
   module ClassMethods
-    def validates_date(*attr_names)
-      configuration = { :message        => "is an invalid date",
-                        :before         => Proc.new { 1.year.from_now.to_date },
-                        :before_message => "must be before %s",
-                        :after          => Date.new(1900, 1, 1),
-                        :after_message  => "must be after %s",
-                        :on => :save }
-      configuration.update(attr_names.pop) if attr_names.last.is_a?(Hash)
-      
-      validates_each(attr_names, configuration) do |record, attr_name, value|
-        value_before_type_cast = record.send("#{attr_name}_before_type_cast")
-        
-        if result = parse_date_string(value_before_type_cast.to_s)
-          if before = configuration[:before]
-            before = before.call(record) if before.is_a?(Proc)
-            unless before.nil?
-              record.errors.add(attr_name, configuration[:before_message] % before) if result > before
+   private
+    [:date, :time].each do |method|
+      class_eval <<-END
+        def relative_#{method}_restrictions(configuration)
+          [:before, :after].collect do |option|
+            [configuration[option]].flatten.compact.collect do |item|
+              case item
+                when Symbol, Proc then item
+                when #{method.to_s.camelize} then item
+                when String then parse_#{method}(item)
+                else raise "\#{item.class}:\#{item} invalid. Use either a Proc, String, Symbol or #{method.to_s.camelize} object."
+              end
             end
           end
-          
-          if after = configuration[:after]
-            after = after.call(record) if after.is_a?(Proc)
-            unless after.nil?
-              record.errors.add(attr_name, configuration[:after_message] % after) if result < after
+        end
+      END
+      
+      class_eval <<-END
+        def #{method}_meets_relative_restrictions(value, record, restrictions, method)
+          restrictions.select do |restriction|
+            case restriction
+              when Symbol then value.send(method, record.send(restriction)) rescue false
+              when Proc   then value.send(method, restriction.call(record)) rescue false
+              when #{method.to_s.camelize} then value.send(method, restriction)
+              else raise
             end
-          end                
+          end.first
+        end
+      END
+    end
+
+    alias_method :relative_date_time_restrictions, :relative_time_restrictions
+    
+    def date_before(value, record, restrictions)
+      date_meets_relative_restrictions(value, record, restrictions, :>)
+    end
+    
+    def date_after(value, record, restrictions)
+      date_meets_relative_restrictions(value, record, restrictions, :<=)
+    end
+    
+    def time_before(value, record, restrictions)
+      time_meets_relative_restrictions(value, record, restrictions, :>)
+    end
+    
+    def time_after(value, record, restrictions)
+      time_meets_relative_restrictions(value, record, restrictions, :<=)
+    end
+    
+    alias_method :date_time_before, :time_before
+    alias_method :date_time_after,  :time_after
+    
+   public
+    [:date, :time, :date_time].each do |validator|
+      class_eval <<-END
+        def validates_#{validator}(*attr_names)
+          configuration = { :message        => "is an invalid #{validator}",
+                            :before_message => "must be before %s",
+                            :after_message  => "must be after %s",
+                            :on => :save }
+          configuration.update(attr_names.pop) if attr_names.last.is_a?(Hash)
+          
+          configuration.assert_valid_keys :message, :before_message, :after_message, :before, :after, :if, :on
+          
+          before_restrictions, after_restrictions = relative_#{validator}_restrictions(configuration)
+          
+          validates_each(attr_names, configuration) do |record, attr_name, value|
+            value_before_type_cast = record.send("\#{attr_name}_before_type_cast")
             
-          record.send("#{attr_name}=", result) unless record.errors.on(attr_name)
-        else
-          record.errors.add(attr_name, configuration[:message])
+            begin
+              result = parse_#{validator}(value_before_type_cast)
+              
+              if failed_restriction = #{validator}_before(result, record, before_restrictions)
+                record.errors.add(attr_name, configuration[:before_message] % failed_restriction)
+              end
+              
+              if failed_restriction = #{validator}_after(result, record, after_restrictions)
+                record.errors.add(attr_name, configuration[:after_message] % failed_restriction)
+              end
+              
+               record.send("\#{attr_name}=", result) unless record.errors.on(attr_name)
+             rescue #{validator.to_s.camelize}ParseError
+              record.errors.add(attr_name, configuration[:message])
+            end
+          end
         end
-      end
-    end
-    
-    def validates_time(*attr_names)
-      configuration = { :message => "is an invalid time", :on => :save }
-      configuration.update(attr_names.pop) if attr_names.last.is_a?(Hash)
-      
-      validates_each(attr_names, configuration) do |record, attr_name, value|
-        value_before_type_cast = record.send("#{attr_name}_before_type_cast")
-        
-        unless value_before_type_cast.is_a?(Time)
-          result = parse_time_string(value_before_type_cast.to_s)
-          record.send("#{attr_name}=", result)
-          record.errors.add(attr_name, configuration[:message]) unless result
-        end 
-      end        
-    end
-    
-    def validates_datetime(*attr_names)
-      configuration = { :message => "is an invalid datetime", :on => :save }
-      configuration.update(attr_names.pop) if attr_names.last.is_a?(Hash)
-      
-      validates_each(attr_names, configuration) do |record, attr_name, value|
-        value_before_type_cast = record.send("#{attr_name}_before_type_cast")
-        
-        unless value_before_type_cast.is_a?(Time)
-          result = parse_datetime_string(value_before_type_cast.to_s)
-          record.send("#{attr_name}=", result)
-          record.errors.add(attr_name, configuration[:message]) unless result
-        end
-      end
+      END
     end
     
    private
-    # Attempt to parse a string into a Date object.
-    # Return nil if parsing fails
-    def parse_date_string(string)
-      return if string.nil?
+    def parse_date(value)
+      raise if value.blank?
+      return value if value.is_a?(Date)
+      raise unless value.is_a?(String)
       
-      year, month, day = case string.strip
-        # 22/1/06
+      year, month, day = case value.strip
+        # 22/1/06 or 22\1\06
         when /^(\d{1,2})[\\\/\.:-](\d{1,2})[\\\/\.:-](\d{2}|\d{4})$/ then [$3, $2, $1]
         # 22 Feb 06 or 1 jun 2001
         when /^(\d{1,2}) (\w{3,9}) (\d{2}|\d{4})$/ then [$3, $2, $1]
@@ -85,18 +120,22 @@ module ActiveRecord::Validations::DateTime
         # 2006-01-01
         when /^(\d{4})-(\d{2})-(\d{2})$/ then [$1, $2, $3]
         # Not a valid date string
-        else return
+        else raise
       end
       
-      Date.new(unambiguous_year(year), month_index(month), day.to_i) rescue nil
+      month, day = day, month if ActiveRecord::Validations::DateTime.us_date_format
+      
+      Date.new(unambiguous_year(year), month_index(month), day.to_i)
+    rescue
+      raise DateParseError
     end
     
-    # Attempt to parse a string into a Time object.
-    # Return nil if parsing fails
-    def parse_time_string(string)
-      return if string.nil?
+    def parse_time(value)
+      raise if value.blank?
+      return value if value.is_a?(Time)
+      raise unless value.is_a?(String)
       
-      hour, minute, second = case string.strip
+      hour, minute, second = case value.strip
         # 12 hour with minute: 7.30pm, 11:20am, 2 20PM
         when /^(\d{1,2})[\. :](\d{2})\s?(am|pm)$/i
           [full_hour($1, $3), $2]
@@ -107,28 +146,32 @@ module ActiveRecord::Validations::DateTime
         when /^(\d{2})[\. :](\d{2})([\. :](\d{2}))?/
           [$1, $2, $4]
         # Not a valid time string
-        else return
+        else raise
       end
       
-      Time.send(ActiveRecord::Base.default_timezone, 2000, 1, 1, hour.to_i, minute.to_i, second.to_i) rescue nil
+      Time.send(ActiveRecord::Base.default_timezone, 2000, 1, 1, hour.to_i, minute.to_i, second.to_i)
+    rescue
+      raise TimeParseError
     end
     
-    # Attempt to parse a string into a date and time
-    # Return nil if parsing fails
-    def parse_datetime_string(string)
-      return if string.nil?
+    def parse_date_time(value)
+      raise if value.blank?
+      return value if value.is_a?(Time)
+      raise unless value.is_a?(String)
       
       # The basic approach is to attempt to parse a date from the front of the string, splitting on spaces.
       # Once a date has been parsed, a time is extracted from the rest of the string.
       split_index = 0
       until false do
-        split_index = string.index(' ', split_index == 0 ? 0 : split_index + 1)
-        break if !split_index or date = parse_date_string(string[0..split_index])
+        split_index = value.index(' ', split_index == 0 ? 0 : split_index + 1)
+        break if !split_index or (date = parse_date(value[0..split_index]) rescue nil)
       end
       
-      time = parse_time_string(string[split_index + 1..string.size]) if split_index
+      time = parse_time(value[split_index + 1..value.size]) if split_index
       
-      Time.send(ActiveRecord::Base.default_timezone, date.year, date.month, date.day, time.hour, time.min, time.sec) rescue nil
+      Time.send(ActiveRecord::Base.default_timezone, date.year, date.month, date.day, time.hour, time.min, time.sec)
+    rescue
+      raise DateTimeParseError
     end
     
     def full_hour(hour, meridian)
