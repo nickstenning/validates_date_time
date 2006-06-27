6ca47c74065a6df5b475c2b402e628302f19d4f8
jonathan <jonathan@20afb1e0-9c0e-0410-9884-91ed27886737>
2006-06-27 00:59:21 +0000

    git-svn-id: http://svn.viney.net.nz/things/rails/plugins/validates_date_time@69 20afb1e0-9c0e-0410-9884-91ed27886737

--- test/date_time_test.rb
+++ test/date_time_test.rb
@@ -4,12 +4,21 @@ class DateTimeTest < Test::Unit::TestCase
   fixtures :people
   
   def test_various_formats
-    { '2006-01-01 01:01:01' => /Jan 01 01:01:01 [\w ]+ 2006/,
-      '1/3/06 7pm'          => /Mar 01 19:00:00 [\w ]+ 2006/,
+    formats = {
+      '2006-01-01 01:01:01' => /Jan 01 01:01:01 [\w ]+ 2006/,
+      '2/2/06 7pm'          => /Feb 02 19:00:00 [\w ]+ 2006/,
       '10 AUG 04 6.23am'    => /Aug 10 06:23:00 [\w ]+ 2004/,
       '6 June 1981 10 10'   => /Jun 06 10:10:00 [\w ]+ 1981/
-    }.each do |value, result|
+    }
+    
+    formats.each do |value, result|
       assert_update_and_match result, :date_and_time_of_birth => value
+    end
+    
+    with_us_date_format do
+      formats.each do |value, result|
+        assert_update_and_match result, :date_and_time_of_birth => value
+      end
     end
   end
   
